c3b83a531950b5c3cbee2e8c36383de0f34bb4c8
Aleksandr Beliaev <cioe@posteo.si>
2023-08-25 22:04:44 +0200

    Added Open Finder selection in Nova hotkey

--- apps/finder.lua
+++ apps/finder.lua
@@ -1,10 +1,15 @@
 local watchApp = require("../utils/watch-app")
 local appname = "Finder"
 
--- Hotkey setup
-local hotkey = hs.hotkey.new({'ctrl'}, "h", nil, function()
+local hotkey1 = hs.hotkey.new({'ctrl'}, "h", nil, function()
   local app = hs.appfinder.appFromName(appname_for_trigger)
   hs.eventtap.event.newKeyEvent({ "shift", "cmd" }, ".", true):post(app)
 end, nil, nil)
 
-watchApp.startAppWatcher({ appname }, { hotkey })
+local hotkey2 = hs.hotkey.new({'ctrl', 'alt'}, "c", nil, function()
+  local app = hs.appfinder.appFromName(appname)
+  app:selectMenuItem({"File", "Open With", "Nova"})
+  app:selectMenuItem({"File", "Open With", "Nova (default)"})
+end, nil, nil)
+
+watchApp.startAppWatcher({ appname }, { hotkey1, hotkey2 })
--- apps/safari.lua
+++ apps/safari.lua
@@ -1,15 +1,8 @@
 local watchApp = require("../utils/watch-app")
 local appname = "Safari"
 
--- Hotkey setup
-local hotkey1 = hs.hotkey.new({'alt', 'ctrl', 'cmd'}, "escape", nil, function()
-  local app = hs.appfinder.appFromName(appname)
-  app:selectMenuItem({"File", "New Window"})
-  -- hs.eventtap.event.newKeyEvent({ "shift", "cmd" }, "m", true):post(app)
-end, nil, nil)
-
-local hotkey2 = hs.hotkey.new({'cmd'}, "q", nil, function()
+local hotkey1 = hs.hotkey.new({'cmd'}, "q", nil, function()
   hs.osascript.applescriptFromFile("apple-scripts/safari-quit-confirmation.applescript")
 end, nil, nil)
 
-watchApp.startAppWatcher({ appname }, { hotkey1, hotkey2 })
+watchApp.startAppWatcher({ appname }, { hotkey1 })
