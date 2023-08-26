c7a992899858ae994210e13c4a5f539318eb75a6
Aleksandr Beliaev <cioe@posteo.si>
2023-08-26 09:31:47 +0300

    Center mouse on a window when focusing/launching certain apps

--- openapps.lua
+++ openapps.lua
@@ -1,25 +1,26 @@
 local centerMouse = require("utils/center-mouse")
--- TBD: does not yet support centering on a window. See https://github.com/eldesperado/.dotfiles/blob/d18deca903b06d300949041f844a3c4b04823685/setup/hammerspoon/utils/mouse.lua#L9
--- centerMouse.centerMouse()
 
-function open_app(name)
+local function open_app(name, centerCursor)
+    centerCursor = centerCursor or false
     return function()
         hs.application.launchOrFocus(name)
         if name == 'Finder' then
             hs.appfinder.appFromName(name):activate()
         end
+        if centerCursor then
+            centerMouse.centerMouse()
+        end
     end
 end
 
 --- quick open applications
-hs.hotkey.bind({}, "f6", open_app("Music"))
+hs.hotkey.bind({}, "f6", open_app("Music", true))
 hs.hotkey.bind({"alt", "cmd"}, "c", open_app("Nova"))
-hs.hotkey.bind({"ctrl", "cmd"}, "s", open_app("Safari"))
 hs.hotkey.bind({"alt", "cmd"}, "s", open_app("Safari"))
 hs.hotkey.bind({"alt", "cmd"}, "z", open_app("Figma"))
 hs.hotkey.bind({"alt", "cmd"}, "m", open_app("Mail"))
 hs.hotkey.bind({"alt", "cmd", "shift"}, "f", open_app("Fork"))
 hs.hotkey.bind({"ctrl", "alt", "cmd"}, "c", open_app("Google Chrome"))
-hs.hotkey.bind({"alt", "cmd"}, "e", open_app("Microsoft Teams"))
+hs.hotkey.bind({"alt", "cmd"}, "e", open_app("Microsoft Teams", true))
 hs.hotkey.bind({"alt", "cmd"}, "t", open_app("iTerm"))
-hs.hotkey.bind({"alt", "cmd", "shift"}, ",", open_app("System Settings"))
+hs.hotkey.bind({"alt", "cmd", "shift"}, ",", open_app("System Settings", true))
--- utils/center-mouse.lua
+++ utils/center-mouse.lua
@@ -1,11 +1,17 @@
 local util = {}
 
 function util.centerMouse()
-    local screen = hs.mouse.getCurrentScreen()
-    -- local nextScreen = screen:next()
-    local rect = screen:fullFrame()
-    local center = hs.geometry.rectMidPoint(rect)
-    hs.mouse.absolutePosition(center)
+  local cwin = hs.window.focusedWindow()
+  local wf = cwin:frame()
+  local cscreen = cwin:screen()
+  local cres = cscreen:fullFrame()
+  if cwin then
+    -- Center the cursor one the focused window
+    hs.mouse.setAbsolutePosition({x=wf.x+wf.w/2, y=wf.y+wf.h/2})
+  else
+    -- Center the cursor on the screen
+    hs.mouse.setAbsolutePosition({x=cres.x+cres.w/2, y=cres.y+cres.h/2})
+  end
 end
 
 return util
