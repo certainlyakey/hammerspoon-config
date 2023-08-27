9bfb505d728c0210cac814b1b6c1279541583bf0
Aleksandr Beliaev <cioe@posteo.si>
2023-08-27 23:05:36 +0300

    Added markdown mode

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ local doubleTapModifiers = require("double-tap-modifiers")
 local appSafari = require("apps/safari")
 local appFinder = require("apps/finder")
 local appBrowsers = require("apps/browsers")
+local markdownMode = require("markdown-mode")
 
 -- Links:
 -- https://blog.jverkamp.com/2023/03/19/keyboard-chords-with-hammerspoon/
---
+++ markdown-mode.lua
@@ -0,0 +1,136 @@
+-- From https://github.com/jasonrudolph/keyboard/blob/e5e351f1cc80f62cca2ce688a5d4a3dd7f3a4b36/hammerspoon/markdown.lua
+
+local isAppFocused = function(name)
+  local app = hs.application.frontmostApplication()
+  local appName = app:name()
+
+  return appName == name
+end
+
+local keyUpDown = function(modifiers, key)
+  -- Un-comment & reload config to log each keystroke that we're triggering
+  -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
+
+  hs.eventtap.keyStroke(modifiers, key, 0)
+end
+
+local function wrapSelectedText(wrapCharacters)
+  -- Preserve the current contents of the system clipboard
+  local originalClipboardContents = hs.pasteboard.getContents()
+
+  -- Copy the currently-selected text to the system clipboard
+  keyUpDown('cmd', 'c')
+
+  -- Allow some time for the command+c keystroke to fire asynchronously before
+  -- we try to read from the clipboard
+  hs.timer.doAfter(0.2, function()
+    -- Construct the formatted output and paste it over top of the
+    -- currently-selected text
+    local selectedText = hs.pasteboard.getContents()
+    local wrappedText = wrapCharacters .. selectedText .. wrapCharacters
+    hs.pasteboard.setContents(wrappedText)
+    keyUpDown('cmd', 'v')
+
+    -- Allow some time for the command+v keystroke to fire asynchronously before
+    -- we restore the original clipboard
+    hs.timer.doAfter(0.2, function()
+      hs.pasteboard.setContents(originalClipboardContents)
+    end)
+  end)
+
+  hs.timer.doAfter(0.4, function()
+    if isAppFocused('Microsoft Teams') then
+      keyUpDown('', 'delete')
+      hs.eventtap.keyStrokes(wrapCharacters)
+    end
+  end)
+
+end
+
+local function inlineLink()
+  -- Fetch URL from the system clipboard
+  local linkUrl = hs.pasteboard.getContents()
+
+  -- Copy the currently-selected text to use as the link text
+  keyUpDown('cmd', 'c')
+
+  -- Allow some time for the command+c keystroke to fire asynchronously before
+  -- we try to read from the clipboard
+  hs.timer.doAfter(0.2, function()
+    -- Construct the formatted output and paste it over top of the
+    -- currently-selected text
+    local linkText = hs.pasteboard.getContents()
+    local markdown = '[' .. linkText .. '](' .. linkUrl .. ')'
+    hs.pasteboard.setContents(markdown)
+    keyUpDown('cmd', 'v')
+
+    -- Allow some time for the command+v keystroke to fire asynchronously before
+    -- we restore the original clipboard
+    hs.timer.doAfter(0.2, function()
+      hs.pasteboard.setContents(linkUrl)
+    end)
+  end)
+end
+
+--------------------------------------------------------------------------------
+-- Define Markdown Mode
+--
+-- Markdown Mode allows you to perform common Markdown-formatting tasks anywhere
+-- that you're editing text. Use Control+m to turn on Markdown mode. Then, use
+-- any shortcut below to perform a formatting action. For example, to format the
+-- selected text as bold in Markdown, hit Control+m, and then b.
+--
+--   b => wrap the selected text in double asterisks ("b" for "bold")
+--   c => wrap the selected text in backticks ("c" for "code")
+--   i => wrap the selected text in single asterisks ("i" for "italic")
+--   s => wrap the selected text in double tildes ("s" for "strikethrough")
+--   l => convert the currently-selected text to an inline link, using a URL
+--        from the clipboard ("l" for "link")
+--------------------------------------------------------------------------------
+
+local markdownMode = hs.hotkey.modal.new({}, 'F20')
+
+local alertStyle = { atScreenEdge = 2, textFont = 'Avenir', radius = 12 }
+
+markdownMode.entered = function()
+  hs.alert.show('Markdown mode started', alertStyle)
+end
+markdownMode.exited = function()
+  hs.alert.show('Markdown mode exited', alertStyle)
+end
+
+-- Bind the given key to call the given function and exit Markdown mode
+function markdownMode.bindWithAutomaticExit(mode, key, fn)
+  mode:bind({}, key, function()
+    mode:exit()
+    fn()
+  end)
+end
+
+markdownMode:bindWithAutomaticExit('b', function()
+  wrapSelectedText('*')
+end)
+
+markdownMode:bindWithAutomaticExit('i', function()
+  wrapSelectedText('_')
+end)
+
+markdownMode:bindWithAutomaticExit('s', function()
+  wrapSelectedText('~')
+end)
+
+markdownMode:bindWithAutomaticExit('l', function()
+  inlineLink()
+end)
+
+markdownMode:bindWithAutomaticExit('`', function()
+  wrapSelectedText('`')
+end)
+
+-- Use Control+m to toggle Markdown Mode
+hs.hotkey.bind({'ctrl'}, 'm', function()
+  markdownMode:enter()
+end)
+markdownMode:bind({'ctrl'}, 'm', function()
+  markdownMode:exit()
+end)
