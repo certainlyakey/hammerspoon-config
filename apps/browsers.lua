791fe42cb29f0254cd4facaa28fdd5b975afb10d
Aleksandr Beliaev <cioe@posteo.si>
2023-08-27 23:06:58 +0300

    Removed temporarily support for many apps at once to copy current tab URL from due to imperfections in the watchApp module

--- apps/browsers.lua
+++ apps/browsers.lua
@@ -1,5 +1,7 @@
 local watchApp = require("../utils/watch-app")
-local appnames = { "Safari", "Google Chrome", "Vivaldi" }
+-- TODO: support for many apps at once is not perfect – alternating apps fail to enable/disable their shortcuts in correct sequence when switching from app to app
+-- local appnames = { "Safari", "Google Chrome", "Vivaldi" }
+local appnames = { "Safari" }
 
 local hotkey1 = hs.hotkey.new({'cmd', 'ctrl'}, "c", nil, function()
   hs.osascript.applescriptFromFile(hs.fs.currentDir() .. "/apple-scripts/copy-url.applescript")
