3cb9aeabd55d94b00ca636da4b5c4ca5ac1c0264
Aleksandr Beliaev <cioe@posteo.si>
2023-08-27 23:05:55 +0300

    Added empty playground module

--- init.lua
+++ init.lua
@@ -7,6 +7,8 @@ local appSafari = require("apps/safari")
 local appFinder = require("apps/finder")
 local appBrowsers = require("apps/browsers")
 local markdownMode = require("markdown-mode")
+local playground = require("playground")
 
 -- Links:
 -- https://blog.jverkamp.com/2023/03/19/keyboard-chords-with-hammerspoon/
+-- https://github.com/muescha/dot_hammerspoon/blob/886d7f33b441357365a226a9e3781fcaeef15787/Functions/HighLight.lua
