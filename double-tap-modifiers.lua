46a72368320163f52c4a1da0ed216d30b01adff2
Aleksandr Beliaev <cioe@posteo.si>
2023-08-29 00:01:49 +0300

    Moved double tap logic into separate utility function

--- double-tap-modifiers.lua
+++ double-tap-modifiers.lua
@@ -1,71 +1,7 @@
--- Copied from https://github.com/jhkuperus/dotfiles/blob/master/hammerspoon/double-shift.lua
-local eventtap = require("hs.eventtap")
-local timer = require("hs.timer")
+local doubleTapWatcher = require("utils/double-tap")
 
-local key = 'shift'
+-- Shortcut: Open Spotlight by pressing shift twice
+local openSpotlight = doubleTapWatcher.doubleTapWatcher('shift', {"fn", "cmd", "alt", "shift", "ctrl"}, 'f1')
 
-local events = eventtap.event.types
-
-local module = {}
-
-
--- What is the maximum number of seconds over which the double-press will be considered a double press
-module.timeFrame = 1
-
--- Change what this function does to change the behavior under double shift (can be overridden when including this module)
-module.action = function()
-  -- On double-tap of shift, we are sending CMD-Space to the OS to activate Spotlight (or whatever is on that shortcut)
-  hs.eventtap.keyStroke({"fn", "cmd", "alt", "shift", "ctrl"}, "f1")
-end
-
-local timeFirstPress, firstDown, secondDown = 0, false, false
-
--- Function to check if the event is "clear" event where no modifier is pressed
-local noFlags = function(ev)
-  local result = true
-  for k, v in pairs(ev:getFlags()) do
-    if v then
-      result = false
-      break
-    end
-  end
-  return result
-end
-
--- Function to check if the event is a shift-press only (no other modifiers down)
-local onlyShift = function(ev)
-  local result = ev:getFlags()[key]
-  for k,v in pairs(ev:getFlags()) do
-    if k~=key and v then
-      result = false
-      break
-    end
-  end
-  return result
-end
-
--- The function actually watching events and keeping track of the shift presses
-module.eventWatcher = eventtap.new({events.flagsChanged, events.keyDown}, function(ev)
-  if (timer.secondsSinceEpoch() - timeFirstPress) > module.timeFrame then
-    timeFirstPress, firstDown, secondDown = 0, false, false
-  end
-
-  if ev:getType() == events.flagsChanged then
-    if noFlags(ev) and firstDown and secondDown then -- shift up and we've seen two, do action
-      if module.action then module.action() end
-      timeFirstPress, firstDown, secondDown = 0, false, false
-    elseif onlyShift(ev) and not firstDown then -- first shift down, start timer
-      firstDown = true
-      timeFirstPress = timer.secondsSinceEpoch()
-    elseif onlyShift(ev) and firstDown then -- second shift down
-      secondDown = true
-    elseif not noFlags(ev) then -- some other key was pressed in between, cancel timer
-      timeFirstPress, firstDown, secondDown = 0, false, false
-    end
-  else -- some other key (other than modifiers) was pressed, cancel timer
-    timeFirstPress, firstDown, secondDown = 0, false, false
-  end
-  return false
-end):start()
-
-return module
+-- Shortcut: Reveal Desktop by pressing alt twice
+-- local revealDesktop = doubleTapWatcher.doubleTapWatcher('cmd', {"fn", "ctrl", "cmd", "shift"}, 'f12')
---
+++ utils/double-tap.lua
@@ -0,0 +1,67 @@
+local eventtap = require("hs.eventtap")
+local timer = require("hs.timer")
+
+local util = {}
+
+function util.doubleTapWatcher(key, mods, keystroke)
+  local module = {}
+
+  local events = eventtap.event.types
+
+  module.timeFrame = 1
+
+  module.action = function()
+    hs.eventtap.keyStroke(mods, keystroke)
+  end
+
+  local timeFirstPress, firstDown, secondDown = 0, false, false
+
+  local noFlags = function(ev)
+    local result = true
+    for k, v in pairs(ev:getFlags()) do
+      if v then
+        result = false
+        break
+      end
+    end
+    return result
+  end
+
+  local onlyKey = function(ev)
+    local result = ev:getFlags()[key]
+    for k,v in pairs(ev:getFlags()) do
+      if k~=key and v then
+        result = false
+        break
+      end
+    end
+    return result
+  end
+
+  module.eventWatcher = eventtap.new({events.flagsChanged, events.keyDown}, function(ev)
+    if (timer.secondsSinceEpoch() - timeFirstPress) > module.timeFrame then
+      timeFirstPress, firstDown, secondDown = 0, false, false
+    end
+
+    if ev:getType() == events.flagsChanged then
+      if noFlags(ev) and firstDown and secondDown then
+        if module.action then module.action() end
+        timeFirstPress, firstDown, secondDown = 0, false, false
+      elseif onlyKey(ev) and not firstDown then
+        firstDown = true
+        timeFirstPress = timer.secondsSinceEpoch()
+      elseif onlyKey(ev) and firstDown then
+        secondDown = true
+      elseif not noFlags(ev) then
+        timeFirstPress, firstDown, secondDown = 0, false, false
+      end
+    else
+      timeFirstPress, firstDown, secondDown = 0, false, false
+    end
+    return false
+  end):start()
+
+  return module
+end
+
+return util
