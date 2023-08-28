1adcebe4cbdf5583b36f94f0218af8813fe763a5
Aleksandr Beliaev <cioe@posteo.si>
2023-08-28 20:37:29 +0300

    Added Hide notifications command

--- init.lua
+++ init.lua
@@ -7,8 +7,13 @@ local appSafari = require("apps/safari")
 local appFinder = require("apps/finder")
 local appBrowsers = require("apps/browsers")
 local markdownMode = require("markdown-mode")
+local other = require("other")
 local playground = require("playground")
 
 -- Links:
 -- https://blog.jverkamp.com/2023/03/19/keyboard-chords-with-hammerspoon/
 -- https://github.com/muescha/dot_hammerspoon/blob/886d7f33b441357365a226a9e3781fcaeef15787/Functions/HighLight.lua
+-- https://balatero.com/writings/hammerspoon/retrieving-input-field-values-and-cursor-position-with-hammerspoon/
+-- https://rakhesh.com/coding/using-hammerspoon-to-switch-apps/ - tap cmd+alt+shortcut twice to do x
+-- https://evantravers.com/articles/2020/06/12/hammerspoon-handling-windows-and-layouts/
+-- https://github.com/asmagill/hammerspoon-config-take2/blob/master/_Spoons/LeftRightHotkey.spoon/init.lua
---
+++ other.lua
@@ -0,0 +1,4 @@
+-- Shortcut: Hide notifications
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, "p", function()
+  hs.osascript.javascriptFromFile(hs.fs.currentDir() .. "/jxa-scripts/close-notifications.js")
+end)
