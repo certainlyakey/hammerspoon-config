4e786c9d37b4ffa4e7f6327c6e19ac1486bc88c4
Aleksandr Beliaev <cioe@posteo.si>
2023-08-28 22:16:31 +0300

    Moved all browser-specific commands to window watcher instead of app watcher due to being more stable

--- apps/browsers.lua
+++ apps/browsers.lua
@@ -1,10 +1,15 @@
 local watchApp = require("../utils/watch-app")
 -- TODO: support for many apps at once is not perfect – alternating apps fail to enable/disable their shortcuts in correct sequence when switching from app to app
--- local appnames = { "Safari", "Google Chrome", "Vivaldi" }
-local appnames = { "Safari" }
+local appnames = { "Safari", "Google Chrome" }
 
 local hotkey1 = hs.hotkey.new({'cmd', 'ctrl'}, "c", nil, function()
   hs.osascript.applescriptFromFile(hs.fs.currentDir() .. "/apple-scripts/copy-url.applescript")
 end, nil, nil)
 
-watchApp.startAppWatcher(appnames, { hotkey1 })
+-- watchApp.startAppWatcher(appnames, { hotkey1 })
+
+
+hs.window.filter.new(appnames)
+:subscribe(hs.window.filter.windowFocused,function() hotkey1:enable() end)
+:subscribe(hs.window.filter.windowUnfocused,function() hotkey1:disable() end)
+
