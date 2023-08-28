7f6619b298ac1f74b6bae105ad392d216bcc72ef
Aleksandr Beliaev <cioe@posteo.si>
2023-08-28 20:39:46 +0300

    Added focus or rotate windows script to some of the global app activation commands

--- open-apps.lua
+++ open-apps.lua
@@ -1,24 +1,29 @@
 local centerMouse = require("utils/center-mouse")
+local focusOrNext = require("utils/focus-or-next-window")
 
 local function open_app(name, centerCursor)
-    centerCursor = centerCursor or false
-    return function()
-        hs.application.launchOrFocus(name)
-        if name == 'Finder' then
-            hs.appfinder.appFromName(name):activate()
-        end
-        if centerCursor then
-            centerMouse.centerMouse()
-        end
+  centerCursor = centerCursor or false
+  return function()
+    hs.application.launchOrFocus(name)
+    if name == 'Finder' then
+      hs.appfinder.appFromName(name):activate()
     end
+    if centerCursor then
+      centerMouse.centerMouse()
+    end
+  end
 end
 
 --- quick open applications
 hs.hotkey.bind({}, "f6", open_app("Music", true))
 hs.hotkey.bind({"alt", "cmd"}, "c", open_app("Nova"))
-hs.hotkey.bind({"alt", "cmd"}, "s", open_app("Safari"))
+hs.hotkey.bind({"alt", "cmd"}, "s", function()
+  focusOrNext.focusOrNext("Safari")
+end)
 hs.hotkey.bind({"alt", "cmd"}, "z", open_app("Figma"))
-hs.hotkey.bind({"alt", "cmd"}, "m", open_app("Mail"))
+hs.hotkey.bind({"alt", "cmd"}, "m", function()
+  focusOrNext.focusOrNext("Mail")
+end)
 hs.hotkey.bind({"alt", "cmd"}, "n", open_app("Notes"))
 hs.hotkey.bind({"shift", "alt", "cmd"}, "c", open_app("Calendar"))
 hs.hotkey.bind({"alt", "cmd", "shift"}, "f", open_app("Fork"))
---
+++ utils/focus-or-next-window.lua
@@ -0,0 +1,57 @@
+-- From https://rakhesh.com/coding/using-hammerspoon-to-switch-apps/ with adaptations for Ventura
+local util = {}
+
+function util.focusOrNext(app)
+  local function getFileName(file)
+    local fileName = file:match("[^/]*.app$")
+    return fileName:sub(0, #fileName - 4)
+  end
+
+  local focusedWindow = hs.window.focusedWindow()
+  -- Output of the above is an hs.window object
+
+  -- I can get the application it belongs to via the :application() method
+  -- See https://www.hammerspoon.org/docs/hs.window.html#application
+  local focusedWindowApp = focusedWindow:application()
+  -- This returns an hs.application object
+
+  -- Get the name of this application; this isn't really useful fof us as launchOrFocus needs the app name on disk
+  -- I do use it below, further on...
+  local focusedWindowAppName = focusedWindowApp:name()
+
+  -- This gives the path - /Applications/<application>.app
+  local focusedWindowPath = focusedWindowApp:path()
+
+  -- I need to extract <application> from that
+  local appNameOnDisk = getFileName(focusedWindowPath)
+
+  -- If already focused, try to find the next window
+  if focusedWindow and appNameOnDisk == app then
+    -- hs.application.get needs the name as per hs.application:name() and not the name on disk
+    -- It can also take pid or bundle, but that doesn't help here
+    -- Since I have the name already from above, I can use that though
+    local appWindows = hs.application.get(focusedWindowAppName):allWindows()
+
+    -- https://www.hammerspoon.org/docs/hs.application.html#allWindows
+    -- A table of zero or more hs.window objects owned by the application. From the current space.
+
+    if #appWindows > 0 then
+        -- It seems that this list order changes after one window get focused,
+        -- Let's directly bring the last one to focus every time
+        -- https://www.hammerspoon.org/docs/hs.window.html#focus
+        if app == "Finder" then
+          -- If the app is Finder the window count returned is one more than the actual count, so I subtract
+          appWindows[#appWindows-1]:focus()
+        else
+          appWindows[#appWindows]:focus()
+        end
+    else
+        -- this should not happen, but just in case
+        hs.application.launchOrFocus(app)
+    end
+  else -- if not focused
+    hs.application.launchOrFocus(app)
+  end
+end
+
+return util
