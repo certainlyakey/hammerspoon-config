71f7525e9deebdebc3e36ed4ccdc5609f92631d6
Aleksandr Beliaev <cioe@posteo.si>
2023-08-29 23:04:58 +0300

    Left click supports multiple clicks now

--- other.lua
+++ other.lua
@@ -1,12 +1,15 @@
+local multiClick = require('utils/multi-clicker')
+
 -- Shortcut: Hide notifications
 hs.hotkey.bind({'ctrl', 'cmd', 'shift'}, 'p', function()
   hs.osascript.javascriptFromFile(hs.fs.currentDir() .. '/jxa-scripts/close-notifications.js')
 end)
 
 -- Shortcut: Click
-hs.hotkey.bind({}, "§", function()
-  local point = hs.mouse.absolutePosition()
-  local clickState = hs.eventtap.event.properties.mouseEventClickState
-  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types["leftMouseDown"], point):setProperty(clickState, 1):post()
-  hs.eventtap.event.newMouseEvent(hs.eventtap.event.types["leftMouseUp"], point):setProperty(clickState, 1):post()
+-- in contrast to newMouseEvent, supports multiple clicks
+local leftClicker = multiClick('leftMouse')
+
+hs.hotkey.bind({}, '§', function()
+  leftClicker.clickDown()
+  leftClicker.clickUp()
 end)
---
+++ utils/multi-clicker.lua
@@ -0,0 +1,77 @@
+-- from https://github.com/msolomon/griddle/blob/3dac49f9be743036e1299f83704b3f7b19ca6a49/Griddle.spoon/multiclicker.lua
+local function Stack(list)
+  local self = {stack = list or {}}
+
+  function self.push(item)
+    self.stack[#self.stack + 1] = item
+  end
+
+  function self.pop()
+    if #self.stack > 0 then
+      return table.remove(self.stack, #self.stack)
+    end
+  end
+
+  function self.peek()
+    return self.stack[#self.stack]
+  end
+
+  function self.size()
+    return #self.stack
+  end
+
+  return self
+end
+
+return function(button)
+  local self = {
+    clicks = 0,
+    clicked = Stack(),
+  }
+  local clickState = hs.eventtap.event.properties.mouseEventClickState
+  local buttonDown = hs.eventtap.event.types[button .. 'Down'] -- e.g. leftMouseDown
+  local buttonUp = hs.eventtap.event.types[button .. 'Up'] -- e.g. leftMouseUp
+  local dragged = hs.eventtap.event.types[button .. 'Dragged'] -- e.g. leftMouseDragged
+  local countResetter = hs.timer.delayed.new(
+    hs.eventtap.doubleClickInterval(),
+    function()
+      if self.clicked.size() == 0 then self.clicks = 0 end
+    end
+  )
+
+  function clickAt(event, clicks, point)
+    hs.eventtap.event.newMouseEvent(event, point):setProperty(clickState, clicks):post()
+  end
+
+  function self.clickDown()
+    local point = hs.mouse.getAbsolutePosition()
+    local numClicks = self.clicks + 1
+    self.clicks = numClicks
+    self.clicked.push({
+      point = point,
+      numClicks = numClicks,
+    })
+    clickAt(buttonDown, numClicks, point)
+  end
+
+  function self.clickUp()
+    countResetter:start()
+    local click = self.clicked.pop()
+    if click then
+      local point = hs.mouse.getAbsolutePosition()
+      if click.point.x ~= point.x or click.point.y ~= point.y then
+        clickAt(dragged, click.numClicks, point)
+      end
+      clickAt(buttonUp, click.numClicks, point)
+    end
+  end
+
+  function self.dragged(point)
+    if self.clicked.size() > 0 then
+      point = point or hs.mouse.getAbsolutePosition()
+      clickAt(dragged, self.clicks, point)
+    end
+  end
+
+  return self
+end
