c6f45344b3e4ec0044beff418a263c9769e3399b
Aleksandr Beliaev <cioe@posteo.si>
2023-09-05 01:35:05 +0300

    Replaced automatic Hammerspoon reload with manual shortcut-enabled

--- hammerspoon-load.lua
+++ hammerspoon-load.lua
@@ -1,14 +1,33 @@
--- Setup automatic configuration reload to make life easier...
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
+local appWatcher = nil
+
+local function reloadConfig()
+  appWatcher:stop()
+  appWatcher = nil
+  hs.reload()
+  hs.notify.new({title='Hammerspoon', informativeText='Configuration loaded'}):send()
 end
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.notify.new({title="Hammerspoon", informativeText="Configuration loaded"}):send()
+
+function sleepCallback( eventType )
+  hs.timer.doAfter(1,hs.reload)
+end
+
+sleepWatcher = hs.caffeinate.watcher.new(sleepCallback):start()
+
+-- configFileWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig)
+-- configFileWatcher:start()
+hs.hotkey.bind({'alt', 'shift', 'ctrl'}, 'r', function()
+  reloadConfig()
+end)
+
+-- Callback function for application events
+function applicationWatcher(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.activated) then
+    if (appName == 'Finder') then
+      -- Bring all Finder windows forward when one gets activated
+      appObject:selectMenuItem({'Window', 'Bring All to Front'})
+    end
+  end
+end
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
