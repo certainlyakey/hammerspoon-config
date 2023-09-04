17435a7b28903ac2f210fbbb7a0bc713f3f1044a
Aleksandr Beliaev <cioe@posteo.si>
2023-09-05 01:32:38 +0300

    Added centering mouse at current window when double-tapping cmd

--- double-tap-modifiers.lua
+++ double-tap-modifiers.lua
@@ -1,9 +1,15 @@
 local doubleTapWatcher = require('utils/double-tap')
+local centerMouse = require('utils/center-mouse')
 
 -- Shortcut: Open Spotlight by pressing shift twice
 local openSpotlight = doubleTapWatcher.doubleTapWatcher('shift', {'fn', 'cmd', 'alt', 'shift', 'ctrl'}, 'f1', false)
 
+-- Shortcut: Center mouse at current window by pressing cmd twice
+local centerMouseAtWindow = doubleTapWatcher.doubleTapWatcher('cmd', {}, '', function()
+  centerMouse()
+end)
+
 -- Shortcut: Reveal Desktop by pressing alt twice
-local revealDesktop = doubleTapWatcher.doubleTapWatcher('alt', {'fn', 'ctrl', 'cmd', 'shift'}, 'f12', function()
+local revealDesktop = doubleTapWatcher.doubleTapWatcher('alt', {}, '', function()
   hs.execute('/System/Applications/Mission\\ Control.app/Contents/MacOS/Mission\\ Control 1')
 end)
