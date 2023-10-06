8ee9eade2015a81de3ba9d3c73067b646c57a168
Aleksandr Beliaev <cioe@posteo.si>
2023-10-07 00:20:58 +0300

    Added Toggle Track move row-by-row action

--- other.lua
+++ other.lua
@@ -2,3 +2,15 @@
 hs.hotkey.bind({'ctrl', 'cmd', 'shift'}, 'p', function()
   hs.osascript.javascriptFromFile(hs.fs.currentDir() .. '/jxa-scripts/close-notifications.js')
 end)
+
+-- Shortcut: Move window lower for Toggl Track
+hs.hotkey.bind({'ctrl', 'cmd', 'alt'}, ';', function()
+  local win = hs.window'aleksandrb'
+  
+  if win ~= nil then
+    local f = win:frame()
+    f.y = f.y + 50
+    win:setFrame(f)
+  end
+
+end)
