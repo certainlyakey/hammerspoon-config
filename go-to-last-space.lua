b9d12e2545fc31836275cb00fc4c12bd841b41e8
Aleksandr Beliaev <cioe@posteo.si>
2024-01-07 01:03:52 +0200

    Switched to a method that allows dragging windows when activating Go to last space shortcut

--- go-to-last-space.lua
+++ go-to-last-space.lua
@@ -1,16 +1,24 @@
+local function goToSpaceByNumber(spaceIndex)
+  hs.timer.doAfter(0.3, function()
+    hs.eventtap.keyStroke({ 'shift', 'ctrl', 'cmd', 'alt' }, tostring(spaceIndex)) 
+  end)
+end
+
 local before_last_space_id = 0;
 local last_space_id = 0;
 
 local lastSpaceWatcher = hs.spaces.watcher.new(function(_)
   last_space_id = before_last_space_id
   before_last_space_id = hs.spaces.focusedSpace()
-  print('current space id is ' .. before_last_space_id)
 end)
 
 lastSpaceWatcher:start()
 
 hs.hotkey.bind({ 'ctrl' }, '`', function()
-  print('last space is ' .. last_space_id)
-  hs.spaces.gotoSpace(last_space_id)
-  -- hs.eventtap.event.newKeyEvent({ 'shift', 'ctrl', 'cmd', 'alt' }, tostring(last_space_id), true):post()
+  local screen = hs.screen.mainScreen()
+  local space_ids = hs.spaces.spacesForScreen(screen:id())
+  local focused_space_index = hs.fnutils.indexOf(space_ids, last_space_id)
+  -- This also works, but doesn't allow to drag windows when switching
+  -- hs.spaces.gotoSpace(last_space_id)
+  goToSpaceByNumber(focused_space_index)
 end)
