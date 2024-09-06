ca881713028caed2a3094ea19d2771a3fb0f9a57
Aleksandr Beliaev <cioe@posteo.si>
2024-09-07 01:32:50 +0300

    Updated double key tap script to be compatible with Sonoma

--- double-tap-modifiers.lua
+++ double-tap-modifiers.lua
@@ -10,6 +10,7 @@ local centerMouseAtWindow = doubleTapWatcher.doubleTapWatcher('cmd', {}, '', 104
 end)
 
 -- Shortcut: Reveal Desktop by pressing right alt twice
+-- This is a Sonoma-specific command
 local revealDesktop = doubleTapWatcher.doubleTapWatcher('alt', {}, '', 524608, function()
-  hs.execute('/System/Applications/Mission\\ Control.app/Contents/MacOS/Mission\\ Control 1')
+  hs.execute('open -a /System/Applications/Mission\\ Control.app/Contents/MacOS/Mission\\ Control  --args 1')
 end)
