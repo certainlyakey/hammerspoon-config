cb8a28b42ccc5d092c4fcae35e9d706544f9d8b2
Aleksandr Beliaev <cioe@posteo.si>
2024-11-20 16:02:48 +0200

    Adapted a script for closing notifications with a shortcut for Sequoia

--- jxa-scripts/close-notifications.js
+++ jxa-scripts/close-notifications.js
@@ -1,54 +1,10 @@
-// Modified from
-// https://gist.github.com/lancethomps/a5ac103f334b171f70ce2ff983220b4f
-"use strict";
-
-function run() {
-  const CurrentApplication = (() => {
-    const app = Application.currentApplication();
-    app.includeStandardAdditions = true;
-    return app;
-  })();
-  const SystemEvents = Application("System Events");
-  const NotificationCenter =
-    SystemEvents.processes.byName("NotificationCenter");
-  const macOSSequoiaOrGreater =
-    parseFloat(CurrentApplication.systemInfo().systemVersion) >= 15.0;
-  const notificationGroups = () => {
-    const windows = NotificationCenter.windows;
-    if (windows.length === 0) {
-      return [];
-    }
-
-    return macOSSequoiaOrGreater
-      ? windows
-          .at(0)
-          .groups.at(0)
-          .groups.at(0)
-          .scrollAreas.at(0)
-          .uiElements.at(0)
-          .buttons()
-      : windows.at(0).groups.at(0).scrollAreas.at(0).uiElements.at(0).groups();
-  };
-
-  const findCloseAction = group => {
-    const [closeAllAction, closeAction] = group.actions().reduce(
-      (matches, action) => {
-        switch (action.description()) {
-          case "Clear All":
-            return [action, matches[1]];
-          case "Close":
-            return [matches[0], action];
-          default:
-            return matches;
-        }
-      },
-      [null, null]
-    );
-    return closeAllAction ?? closeAction;
-  };
-
-  const actions = notificationGroups().map(findCloseAction);
-  for (const action of actions) {
-    action?.perform();
-  }
-}
+// see https://community.folivora.ai/t/close-all-notification-alerts-notification-center-is-not-working-on-macos-sequoia-15-0/39200/34
+Application("System Events")
+  .applicationProcesses.byName("NotificationCenter")
+  .windows[0]
+  .groups[0]
+  .groups[0]
+  .scrollAreas[0]
+  .uiElements()
+  .map(banner => banner.actions().slice(-1)[0])
+  .forEach(banner => banner.perform())
