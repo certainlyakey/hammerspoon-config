a006bbea38fe9e8230d61d14277078444bf58592
Aleksandr Beliaev <cioe@posteo.si>
2025-02-10 22:07:12 +0200

    Fixed accidental trigger of the action by pressing a number key when switching to a space using § modifier

--- go-to-space.lua
+++ go-to-space.lua
@@ -1,25 +1,37 @@
 -- Double-tap of the modifier produces the original key
-local function goToSpaceByNumber(spaceIndex)
-  -- local all_spaces = hs.spaces.spacesForScreen(hs.screen.mainScreen():id())
-  -- local new_space_id = all_spaces[spaceIndex]
-  -- hs.spaces.gotoSpace(new_space_id)
-  hs.eventtap.event.newKeyEvent({ 'shift', 'ctrl', 'cmd', 'alt' }, tostring(spaceIndex), true):post()
-end
-
 -- from https://groups.google.com/g/hammerspoon/c/HgDHNAWupFU/m/hny2NN8FCAAJ
 local modifierKey = '§'
 local k = hs.hotkey.modal.new()
-local triggerK = hs.hotkey.bind('', modifierKey, function() k:enter() end)
+local triggerK = hs.hotkey.bind('', modifierKey, function() 
+    k:enter() 
+end)
 
--- Shortcut: Go to space by pressing a custom modifier (§) and number key
-for i = 1, 6 do
-  k:bind('', tostring(i), function() goToSpaceByNumber(i) end, function() k:exit() end)
+-- Function to handle space switching
+local function goToSpaceByNumber(spaceIndex)
+    hs.eventtap.event.newKeyEvent({ 'shift', 'ctrl', 'cmd', 'alt' }, tostring(spaceIndex), true):post()
 end
 
-k:bind('', modifierKey, function()
-  triggerK:disable()
-end, function()
-  hs.eventtap.keyStroke({''}, modifierKey)
-  hs.timer.doAfter(.1, function() triggerK:enable() end)
-  k:exit()
+-- Bind number keys only when in modal state
+for i = 1, 6 do
+    k:bind('', tostring(i), nil, function() 
+        goToSpaceByNumber(i)
+        k:exit()
+    end)
+end
+
+-- Shortcut: Go to space by pressing a custom modifier (§) and number key
+k:bind('', modifierKey, nil, function()
+    triggerK:disable()
+    hs.eventtap.keyStroke({''}, modifierKey)
+    hs.timer.doAfter(0.1, function() 
+        triggerK:enable() 
+    end)
+    k:exit()
 end)
+
+-- Automatically exit modal after 2 seconds if no key is pressed
+k.entered = function(self)
+    hs.timer.doAfter(2, function() 
+        k:exit() 
+    end)
+end
